ed7c0de091f18d17191e43560c42cdf810fd4ff3
Tokuty <sokoninoruna@gmail.com>
2025-01-04 10:23:51 +0900

    GoodButton #10

--- src/Transition.jsx
+++ src/Transition.jsx
@@ -3,12 +3,14 @@ import ButtonClickChangeColor from '../src/pages/ButtonClickChangeColor'
 import ValueDisplayEntered from '../src/pages/ValueDisplayEntered'
 import Counter from '../src/pages/Counter'
 import ShowList from '../src/pages/ShowList'
+import GoodButton from '../src/pages/GoodButton'
 
 const linkList = {
     "/ButtonClickChangeColor":"ボタンを押下したら文字の色が切り替わる",
     "/ValueDisplayEntered":"入力フィールドからの値を表示する",
     "/Counter":"カウントアップするボタン",
-    "/ShowList":"リストを受け取って表示する"
+    "/ShowList":"リストを受け取って表示する",
+    "/GoodButton":"Goodボタン"
 }
 
 const routeList = {
@@ -16,7 +18,8 @@ const routeList = {
     "/ButtonClickChangeColor":<ButtonClickChangeColor/>,
     "/ValueDisplayEntered":<ValueDisplayEntered/>,
     "/Counter":<Counter/>,
-    "/ShowList":<ShowList/>
+    "/ShowList":<ShowList/>,
+    "/GoodButton":<GoodButton/>
 }
 
 export { linkList, routeList };
